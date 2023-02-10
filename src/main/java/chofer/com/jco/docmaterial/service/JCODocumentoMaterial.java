e268f84d44160051238313c2c64e101bfffad3d6
brandoncsti <barango@csticorp.biz>
2023-02-10 17:44:24 -0500

    RFC - PRACTICA

--- src/main/java/chofer/com/jco/docmaterial/service/JCODocumentoMaterial.java
+++
@@ -1,8 +0,0 @@
-package chofer.com.jco.docmaterial.service;
-
-import com.incloud.hcp.jco.balanza.Carreta.dto.CarretaUpdateResponseDTO;
-
-public interface JCODocumentoMaterial {
-    //    void extraerCarretaListRFC(String sign, String option, String low, String high) throws Exception;
-
-}
